64fd70b978a9cfa02bc2774c092d8eb53aaccc4b
yevhen <yevhenstore@gmail.com>
2022-05-24 18:53:00 +0300

    строки списки и словари

---
+++ 00/strinc_list_dict.py
@@ -0,0 +1,124 @@
+
+def print_data (some_data):
+    print(some_data)
+
+hello_world = "Hello world" 
+
+
+# nuber_list = ("one", "two",  "three", "four", "five")
+
+
+# Если цвет синий, печатаем мы нашли синий цвет
+# colore_list = ("Красный", "Зеленый","Синий","Желтый", "Синий")
+# favorite_colore = ("Синий", "Зеленый")
+# count = 0 
+# colore_count = {}
+
+# for colore in colore_list:
+#     if colore in favorite_colore:
+#         print("Мы нашли любимый цвет")
+#         colore_count[colore] = colore_count.get(colore, 0) + 1
+   
+
+# print(colore_count)
+
+
+
+# numbers = "one,two,three,four,five,ten,five,three"
+# number_list = numbers.split(",")
+
+# favorite_numbers = ("five", "two")
+# count = 0 
+# numbers_count = {}
+
+# for i in number_list:
+#     if i in favorite_numbers:
+#         print("мы нашли счастливое число")
+#         numbers_count[i] = numbers_count.get(i, 0) + 1
+
+    
+
+#print(numbers_count)
+
+
+numbers_dict = {
+    "one"  : {
+        "two" : 2,
+        "six_plus_three" : 9, 
+    },
+    "two"  : 3,
+    "three": 3,
+    "four" : 1,
+    "five" : 5,
+    "six"  : 6,
+}
+
+
+print(numbers_dict["one"]["six_plus_three"])
+
+# error = "Тут ничего нет"
+# for i in number_list:
+#     print(numbers_dict.get(i, 0))
+
+# for k in favorite_numbers:
+#     if i == k:
+#         print("мы нашли счастливое число")
+
+
+
+# print_data(type(numbers))
+# print_data(dir(numbers))
+# print_data( numbers.upper() )
+
+
+
+
+# type(any_data) - показывает, какой тип данных у передаваемого объекта
+# dir(any_data)  - посмотреть методы объекта
+
+# Посмотреть что они делают
+# 'capitalize'
+# 'casefold'
+# 'center'
+# 'count'
+# 'encode'
+# 'endswith'
+# 'expandtabs'
+# 'find'
+# 'format'
+# 'format_map'
+# 'index'
+# 'isalnum'
+# 'isalpha'
+# 'isascii'
+# 'isdecimal'
+# 'isdigit'
+# 'isidentifier'
+# 'islower'
+# 'isnumeric'
+# 'isprintable'
+# 'isspace'
+# 'istitle'
+# 'isupper'
+# 'join'
+# 'ljust'
+# 'lower'
+# 'lstrip'
+# 'maketrans'
+# 'partition'
+# 'replace' - Прочитать обязательно
+# 'rfind'
+# 'rindex'
+# 'rjust'
+# 'rpartition'
+# 'rsplit'
+# 'rstrip'
+# 'split'
+# 'splitlines'
+# 'startswith'
+# 'strip'
+# 'swapcase'
+# 'title'
+# 'translate'
+# 'upper'
+# 'zfill'
